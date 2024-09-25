58cf725dac45c69743de2a40993bc0f3c6b4b819
Abdik <alma55497@gmail.com>
2024-09-25 16:35:43 +0300

    Прогресс до 2 формулы и до 7 столбца

--- laba_1.py
+++ laba_1.py
@@ -1,29 +1,48 @@
 from list_command import *
 
 
-list_x = [201, 147, 2613, 532, 1316, 2142, 1325, 212, 436, 227, 340, 633, 1372]
 list_y = [77923.0, 46044.3, 1792048.2, 288951.6, 715050.4, 1000247.6, 538340.2, 52167.8, 118134.7, 69195.3, 126827.2, 141294.8, 541188.0]
+list_x = [201, 147, 2613, 532, 1316, 2142, 1325, 212, 436, 227, 340, 633, 1372]
 
 class Econom:
 
-    def __init__(self, x: list, y: list):
-        self.list_x = x
+    def __init__(self, y: list, x: list):
         self.list_y = y
+        self.list_x = x
         self.list_mean = create_list_mean(y, x)
-        self.list_xy = create_list_xy(x, y, self.list_mean)
-        self.list_x_square = create_list_square(x, self.list_mean)
-        self.list_y_square = create_list_square(y, self.list_mean)
+        self.list_sum = create_list_sum(y, x)
+        self.list_xy = create_list_xy(x, y, self.list_mean, self.list_sum)
+        self.list_x_square = create_list_square(x, self.list_mean, self.list_sum)
+        self.list_y_square = create_list_square(y, self.list_mean, self.list_sum)
+        self.b = None
+        self.a = None
+        Econom.table_analysis(self)
+
+        self.list_Yx = create_Yx(self, self.list_mean, self.list_sum)
+        self.list_y_Yx_square = create_list_y_Yx_square(self, self.list_mean, self.list_sum)
+
+    def table_analysis(self):
+        self.b = create_regress_coeff(self)
+        self.a = create_konst(self)
 
     def __repr__(self):
         return (
-            f"Список x: {self.list_x}\n"
-            f"Список y: {self.list_y}\n"
             f"Список средних значений: {self.list_mean}\n"
+            f"Список сумм значений: {self.list_sum}\n"
+            f"Список y: {self.list_y}\n"
+            f"Список x: {self.list_x}\n"
             f"Список x*y: {self.list_xy}\n"
             f"Список x^2: {self.list_x_square}\n"
             f"Список y^2: {self.list_y_square}\n"
+            f"Список Yx: {self.list_Yx}\n"
+            f"Список y-Yx: {self.list_y_Yx_square}\n"
+            
+            
+            "\n"
+            f"Значение b: {self.b}\n"
+            f"Значение a: {self.a}\n"
         )
 
 
-x_y = Econom(list_x, list_y)
+x_y = Econom(list_y, list_x)
 print(x_y)
--- list_command.py
+++ list_command.py
@@ -1,4 +1,5 @@
 from statistics import mean
+from math import sqrt
 
 
 def create_list_mean(y: list, x: list):
@@ -6,22 +7,64 @@ def create_list_mean(y: list, x: list):
     list_mean = [mean(y), mean(x)]
     return list_mean
 
-def create_list_xy(list_x: list, list_y: list, list_mean: list):
+def create_list_sum(y: list, x: list):
+    """Возвращает среденее значение"""
+    list_sum = [sum(y), sum(x)]
+    return list_sum
+
+def create_list_xy(list_x: list, list_y: list, list_mean: list, list_sum: list):
     """Создаёт и возвращает список y*x"""
     list_xy = []
     for i in range(len(list_x)):
         list_xy.append(list_x[i]*list_y[i])
 
     list_mean.append(mean(list_xy))
+    list_sum.append(sum(list_xy))
     return list_xy
 
-def create_list_square(list_values: list, list_mean: list):
+def create_list_square(list_values: list, list_mean: list, list_sum: list):
     """Создаёт и возвращает список значений в степени"""
     list_values_square = []
     for i in list_values:
         list_values_square.append(i**2)
 
     list_mean.append(mean(list_values_square))
+    list_sum.append(sum(list_values_square))
     return list_values_square
+
+def create_regress_coeff(self):
+    """Вычисляет переменную b"""
+    b = self.list_mean[2] - self.list_mean[0] * self.list_mean[1]
+    c = self.list_mean[3] - self.list_mean[1]**2
+
+    return b/c
+
+def create_konst(self):
+    """Вычисляет переменную a"""
+    a = self.list_mean[0] - self.b * self.list_mean[1]
+
+    return abs(a)
+
+def create_Yx(self, list_mean: list, list_sum: list):
+    """Вычисляет переменную list_Yx"""
+    list_Yx = []
+    for i in self.list_x:
+        list_Yx.append(self.a + self.b * i)
+
+    list_mean.append(mean(list_Yx))
+    list_sum.append(sum(list_Yx))
+    return list_Yx
+
+def create_list_y_Yx_square(self, list_mean: list, list_sum: list):
+    """(y-Yx) в куадрат"""
+    list_y_Yx_square = []
+
+    for i in range(len(self.list_y)):
+        list_y_Yx_square.append((self.list_y[i] - self.list_Yx[i])**2)
+
+    list_mean.append(mean(list_y_Yx_square))
+    list_sum.append(sum(list_y_Yx_square))
+    return list_y_Yx_square
+
 
 
